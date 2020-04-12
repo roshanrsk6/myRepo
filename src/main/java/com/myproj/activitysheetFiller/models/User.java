a746f254433fd060188c915d6cc36a2e1fb92469
Dynatrace User <Dynatrace User@192.168.1.12>
2020-04-12 10:05:50 +0200

    initial import

---
+++ src/main/java/com/myproj/activitysheetFiller/models/User.java
@@ -0,0 +1,41 @@
+package com.myproj.activitysheetFiller.models;
+
+public class User {
+	
+private String dicId;
+private String name;
+private String bensylId;
+private String teamId;
+
+
+
+public String getDicId() {
+	return dicId;
+}
+public void setDicId(String dicId) {
+	this.dicId = dicId;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getBensylId() {
+	return bensylId;
+}
+public void setBensylId(String bensylId) {
+	this.bensylId = bensylId;
+}
+public String getTeamId() {
+	return teamId;
+}
+public void setTeamId(String teamId) {
+	this.teamId = teamId;
+}
+
+
+
+
+	
+}
